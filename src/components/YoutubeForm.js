3fa4ac79d6c572ae6e03093e57ce7cc2182ccc9c
niazyeladawy <niazyadawy2@gmail.com>
2022-11-11 14:46:48 +0200

    refactor code

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,62 +1,61 @@
-import { useFormik } from 'formik'
+import { ErrorMessage, Field, Form, Formik } from 'formik'
 import React from 'react'
 import './youtubeform.module.css'
+import * as Yup from 'yup'
+const initialValues = {
+    name: "",
+    email: "",
+    channel: "",
+}
+
+const onSubmit = ()=>{
+
+}
+
+
+const validate = values => {
+    let errors = {};
+
+    if (!values.name) {
+        errors.name = "Required"
+    }
+
+    if (!values.email) {
+        errors.email = "Required"
+    }
+    else if (! /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(values.email)) {
+        errors.email = "Wrong Email Format"
+    }
+
+    if (!values.channel) {
+        errors.channel = "Required"
+    }
+
+    return errors;
+}
+
+const validationSchema = Yup.object({
+    name:Yup.string().required('Required!'),
+    email:Yup.string().email('Invalid Email').required('Required'),
+    channel:Yup.string().required('Required'),
+})
 
 const YoutubeForm = () => {
-    const initialValues={
-        name:"",
-        email:"",
-        channel:"",
-    }
 
-    const onSubmit=values =>{
-        
-    }
-
-    const validate = values =>{
-        let errors ={};
-
-        if(!values.name){
-            errors.name = "Required"
-        }
-
-        if(!values.email){
-            errors.email = "Required"
-        }
-        else if(! /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(values.email)){
-            errors.email = "Wrong Email Format"
-        }
-
-        if(!values.channel){
-            errors.channel = "Required"
-        }
-
-
-        return errors;
-    }
-
-    const formik = useFormik({
-        initialValues,
-        onSubmit,
-        validate
-    })
-    
-console.log(formik.errors);
     return (
-        <div>
-            <form onSubmit={formik.handleSubmit}>
+        <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}  >
+            <Form >
                 <label htmlFor="name">Name</label>
-                <input type="text"  onBlur={formik.handleBlur} onChange={formik.handleChange} id='name' name='name' value={formik.values.name} />
-                { formik.touched.name &&  formik.errors.name? <span>{formik.errors.name} </span>: null}
-                <label htmlFor="email">Email</label>
-                <input type="email"  onBlur={formik.handleBlur} onChange={formik.handleChange} id='email' name='email'  value={formik.values.email}/>
-                {formik.touched.email  &&  formik.errors.email? <span>{formik.errors.email} </span>:null}
+                <Field type="text"  id='name' name='name'  />
+                <ErrorMessage name='name' />
+                <Field type="email" o id='email' name='email'   />
+                <ErrorMessage name='email' />
                 <label htmlFor="channel">channel</label>
-                <input type="text"  onBlur={formik.handleBlur} onChange={formik.handleChange} id='channel' name='channel' value={formik.values.channel} />
-                {formik.touched.channel &&   formik.errors.channel? <span>{formik.errors.channel} </span>:null}
+                <Field type="text"  id='channel' name='channel'   />
+                <ErrorMessage name='channel' />
                 <button type='submit'>submit</button>
-            </form>
-        </div>
+            </Form>
+        </Formik>
     )
 }
 
